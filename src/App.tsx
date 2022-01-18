686a0539e79abba76bc32a4b4612fd58542a7857
jungwoole91 <jungwoole91@gmail.com>
2022-01-19 00:59:09 +0900

    Babel을 통한 루트 경로 지정 / 아이콘 추가

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@
 
 import React from 'react';
 import {Text} from 'react-native';
-import Root from './screen/RootNavigator';
+import Root from './screens/RootNavigator';
 
 const App = () => {
   return (
